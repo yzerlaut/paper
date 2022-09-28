3f3d47e3427ea166a97fb6200b43c329044596a8
Yann Zerlaut <yann.zerlaut@icm-institute.org>
2022-09-28 15:02:16 +0200

    update

--- finalyz/layers2png.sh
+++ finalyz/layers2png.sh
@@ -1,8 +1,13 @@
+mkdir pngs -p
 readarray array <<< cut -d ' '  -f 3 <<< $(inkscape --actions="select-all:layers;select-list;" $1)
 
+declare -i count=1
 for i in "${array[@]}"
 do
   echo $(cut -d ' ' -f 1 <<< $i)
   myLayer=$(cut -d ' ' -f 1 <<< $i)
-  $(inkscape --export-area-page --actions="export-id-only;export-id:$myLayer;export-filename:$myLayer.png;export-do;" $1)
+  $(inkscape --export-area-page --actions="export-id-only;export-id:$myLayer;export-filename:pngs/layer$count.png;export-do;" $1)
+  # $(inkscape --export-area-page --actions="export-id-only;export-id:$myLayer;export-filename:pngs/$i-$myLayer.png;export-do;" $1)
+  count+=1
+  echo $count
 done
